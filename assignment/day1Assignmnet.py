a4df893b841f75fac35cca180cc4be5c094cb26a
Nishan Tiwari <39886102+NishanTiwari@users.noreply.github.com>
2019-11-02 04:58:18 +0200

    Add files via upload

---
+++ assignment/day1Assignmnet.py
@@ -0,0 +1,39 @@
+
+# # print the beautiful World
+# print("Hello World")
+# # float number print
+# print (27.5/3)
+# # booln expression use to compare
+# print (25>=45) 
+# print (5 == 5)
+
+
+# # assignment 1
+
+
+# #
+# #total wholesale cost of 60 copy??
+
+
+# cover_of_book= float(24.95)
+# shiping_cost= 3
+# each_copy_cost= float(0.73)
+# discount_for_bookkeepers = (24.95*40/100)
+# price_after_discount = cover_of_book-discount_for_bookkeepers
+
+# print( (price_after_discount + shiping_cost + each_copy_cost) * 60)
+# print (f"price of book is ${price_after_discount + shiping_cost + each_copy_cost)* 60}")
+
+# # assignment 2 (area of trapigiod)
+# base_1 = 5
+# base_2 = 6
+# height = int(input("Height of trapezoid: "))
+# base_1 = int(input('Base one value: '))
+# base_2 = int(input('Base two value: '))
+# area = ((base_1 + base_2) / 2) * height
+# print("Area is:", area)
+
+
+
+
+
---
+++ assignment/day2_assignment.py
@@ -0,0 +1,15 @@
+print("which account website you want to see?")
+
+user_input = (input("type 1 for facebook, 2 for gmail, 3 for instagram, 4 for tweeter, e for exit, y to continue, n for quit:   " ))
+
+# # while user_input != "e"
+# if user_input == "1":
+#     print(f"site: {socialMediaAccount['facebook']['email']} \npassword {socialMediaAccount['facebook']['password']}")
+# elif user_input == "2":
+#     print(f"site: {socialMediaAccount['gmail']['email']} \npassword {socialMediaAccount['gmail']['password']}")
+# elif user_input == "3":
+#     print(f"site: {socialMediaAccount['instagram']['email']} \npassword {socialMediaAccount['instagram']['password']}")
+# elif user_input == "4":
+#     print(f"site: {socialMediaAccount['tweeter']['email']} \npassword {socialMediaAccount['tweeter']['password']}")
+# else:
+#print(" Hyy fucker please read givan massage carefully ")
