8288695f195691ef3423e42431ca255d1bc52936
Nishan Tiwari <39886102+NishanTiwari@users.noreply.github.com>
2019-11-02 04:40:04 +0200

    Add files via upload

---
+++ pythonday1.py
@@ -0,0 +1,59 @@
+
+# print Function is use as a console windows
+print("Hello World")
+print ("This is a good example")
+print("But what the hell am I writting now?")
+print("I'm running out of ideas")
+print("F**k ! I'm stucked")
+print("Let's just print this")
+
+#----------------------#-------------------------------------------
+
+# use for comments it is importaint to use as explanation od code to follow with
+
+# print out my lovely hello world program
+print("Hello World")
+
+#------------------------#-----------------------------------------
+
+# Numbers and math
+
+print("2+2 is", 2+2)
+print("2-2 is", 2-2)
+print("4/2 is", 4/2)
+print("4*2 is", 4*2)
+print("5%2 is", 5%2)
+print("2 > 3 is", 2>3)
+print("2 < 3 is", 2<3)
+print("4 >= 4 is", 4>=4)
+print("5 <=5 is", 5>=5)
+
+#---------------------------------#------------------------
+
+
+# variable is nothing more then name for some things. Veriables are used to understand the programme in english
+# viarable rules
+# use camale case, must start letter or underscore, can start with number not with !
+
+apple = 30
+orange = 7
+basket = apple + orange
+bill   = basket
+print("you have bought",apple,"apple")
+print ("you have also bought",orange, "orange")
+
+
+# formating text 
+#  (str.format())  Python's str.format() method of the string class allows you to do variable substitutions and value formatting. 
+#  This lets you concatenate elements together within a string through positional formatting
+# in {} .format stored in 
+print( "your basket have {} fruit in it".format(basket))
+print("You have to pay %d Dollars" %bill)
+
+(kiwi, mandarin, tomato, shop, cheaper) = (2.5, 3.0, 2, "Lidl", True)
+print(kiwi)
+print(mandarin)
+print(tomato)
+print(shop)
+print(cheaper)
+
---
+++ pythonday2.py
@@ -0,0 +1,151 @@
+
+How much = imput ("how much i will pay?")
+
+howmuch = input ("how much i will pay?")
+how much i will pay?
+ 
+ 
+# string method
+ 
+ 
+#string a string at a specific character/location
+new_string = "jacky-chan-20".replace("-","")
+
+#--------------------------------------------#------------------------------------
+# count will count how many time a value appair in the string
+length = len(nwe_string)
+print (length)
+11
+strip = nwe_str.strip()
+print(strip)
+jackychan20
+
+#----------------------------------------------#--------------------------------------------
+
+# string Method 
+# indexing
+str = " hello World"
+f = str[0]
+print (f)
+print(f)
+ 
+str = "Hello World"
+f = str[1]
+Print(f)
+
+
+
+
+
+
+
+
+# intiger
+
+int()
+# float
+float()
+# string  it has a text using ""
+str() 
+# boolean
+bool()
+# These are original data type
+
+#---------------------------------------------#------------------------------------------
+
+
+# non prermitive data types 
+
+# list 
+
+list = ["MANGO", "ORANGE", 1 ,33, "STRING"]
+l = len(list)
+print(l)
+
+list [5] = man 
+print = List[5]
+
+
+
+#---------------------------------------------#---------------------------------------------
+# to incert new valau in end (  pop )
+list.pop()
+print(list)
+
+# incert in specific position  first you need to add the spefic position ( index, new value)
+list.incert(3,455)
+print(list)
+
+# slice 
+slice = list[2:]
+print(slice) 
+
+
+# list backward
+#-------------------------------------------#----------------------------------------------
+
+
+
+
+# list type 2
+# 2 Thupple  (value is unchangable) use  for data that stored not to be chage
+
+tup = (1, 23, 4)
+tup[1] = "cool"
+Print(tup)
+
+# dictinory
+
+dictinory = {
+    "key1" : "value 1",
+    "key2" : "value 2",
+    "key3" : "value 3",
+}
+print(dictinory)
+ # to assign new value in dictionary
+
+dictinory["key5"] : "new value"
+
+
+# if statement ( make input in same format )
+guss_number = int(input ("choose your number,please! "))
+right_answer = 3
+
+if guss_number == right_answer:
+    print ("you are right")
+else:
+    print("wrong")
+
+guss_number  = (input ("choose your number,please! ")
+right_answer = 3
+right_answer = str(right_answer)
+if guss_number == right_answer:
+    print (you are right)
+else: 
+        print(wrong)
+
+
+
+# else if statement
+guss_number = (input ("choose your number,please! ")
+right_answer = 3
+right_answer= str(right_answer)
+if guss_number == 1:
+    print ("no no  highrt")
+    elif: guss_number == 1:
+
+        print(wrong)
+
+
+# loops keep reapiting the code until it match 
+count = 0
+while count < 10:
+    print(count)
+    count = count + 1
+
+print(done)
+
+
+# for looop
+
+
